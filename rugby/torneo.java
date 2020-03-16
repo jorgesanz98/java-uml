b0054b3a628be6ac91d0b76accb67f78862cf101
JorgeSanz <jorgesanz882@gmail.com>
2020-03-16 18:33:04 +0100

    rugby

--- rugby/torneo.java
+++ rugby/torneo.java
@@ -3,4 +3,13 @@ package rugby;
 public class torneo 
 {
 	private String agno;
+
+	public String getAgno() {
+		return agno;
+	}
+
+	public void setAgno(String agno) {
+		this.agno = agno;
+	}
+	
 }
